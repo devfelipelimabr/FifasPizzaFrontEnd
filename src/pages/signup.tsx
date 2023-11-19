10c4e740497ee3f921daefd5a7371696fce764ce
devfelipelimabr <felipelima85@hotmail.com>
2023-11-19 13:51:06 -0300

    Created signup screen-page.

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -8,25 +8,27 @@ import { Button } from "@/components/ui/Button";
 
 import Link from "next/link";
 
-export default function Home() {
+export default function SignUp() {
   return (
     <>
       <Head>
-        <title>Fifas Pizza - Login</title>
+        <title>Fifas Pizza - Cadastro</title>
       </Head>
       <div className="containerCenter">
         <Image src={logoImg} alt="Logo fifas pizzaria" />
         <div className="login">
+          <h1>criando sua conta</h1>
           <form>
+            <Input placeholder="Nome" type="text" />
             <Input placeholder="Email" type="text" />
             <Input placeholder="Senha" type="password" />
 
             <Button type="submit" loading={false}>
-              Acessar
+              cadastrar
             </Button>
           </form>
-          <Link className="text" href="/signup">
-            Não possui uma conta? Cadastre-se
+          <Link className="text" href="/">
+            Já possui uma conta? Faça login
           </Link>
         </div>
       </div>
