2187008a9b08e1a0a74db550373afaf8150984f2
devfelipelimabr <felipelima85@hotmail.com>
2023-11-23 20:29:23 -0300

    Created Header component.

---
+++ src/components/Header.tsx
@@ -0,0 +1,39 @@
+import { useContext } from "react";
+import Link from "next/link";
+import logoImg from "../../public/logo.png";
+import Image from "next/image";
+import { FiLogOut } from "react-icons/fi";
+import { AuthContex } from "@/contexts/AuthContext";
+
+export function Header() {
+  const { signOut } = useContext(AuthContex);
+  
+  return (
+    <header className="headerContainer">
+      <div className="headerContent">
+        <Link href="/dashboard">
+          <Image
+            src={logoImg}
+            alt="Logo fifas pizzaria"
+            priority={true}
+            width={190}
+          />
+        </Link>
+        
+        <nav>
+          <Link href="/category">
+            <p>Categoria</p>
+          </Link>
+
+          <Link href="/carte">
+            <p>Cardápio</p>
+          </Link>
+
+          <button onClick={signOut}>
+            <FiLogOut size={24} />
+          </button>
+        </nav>
+      </div>
+    </header>
+  );
+}
